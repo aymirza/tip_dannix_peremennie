4e17f70d4cbce2118a9cb580f4e501b74c12c6f7
aymirza <49402749+aymirza@users.noreply.github.com>
2021-07-23 09:22:49 +0500

    Initial commit

---
+++ src/main/java/Localnaya_peremennfya.java
@@ -0,0 +1,14 @@
+public class Localnaya_peremennfya {
+
+    public void pupAge(){
+        int age=0;
+        age = age +7;
+        System.out.println("Vozrast shenka = "+age);
+    }
+
+    public static void main(String[] args) {
+        Localnaya_peremennfya age = new Localnaya_peremennfya();
+        age.pupAge();
+    }
+
+}
